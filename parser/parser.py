79a4e061934c670ed72344a730fb06421d0f6cfb
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-13 23:26:55 +0200

    Update parser.py

--- parser/parser.py
+++ parser/parser.py
@@ -1,1 +1,9 @@
-# // 
+class AST(object) :
+    pass
+
+class BinOp(AST):
+    # constructor
+    def __init__(self, left, op, right) :
+        self.left = left
+        self.right = right
+        self.token = self.op = op
