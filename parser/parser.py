9075a7aa90d78e437a09ddde07f665cfe5eed600
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-16 00:11:55 +0200

    Update

--- parser/parser.py
+++ parser/parser.py
@@ -13,18 +13,17 @@ LPAREN = 'LPAREN'
 RPAREN = 'RPAREN'
 EOF = 'EOF'
 
-
 class AST:
     pass
 
-class BinOp(AST):
+class BinOp:
     # constructor
     def __init__(self, left, op, right) :
         self.left = left
         self.token = self.op = op
         self.right = right
 
-class Num(AST):
+class Num:
     def __init__(self,token):
         self.token = token
         self.value = token.value
